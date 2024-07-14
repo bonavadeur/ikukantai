d54d78abee9c1b31c01ea3b55d32bd83de219292
bonavadeur <daodaihiep22ussr@gmail.com>
2024-07-14 15:04:00 +0700

    v1.1-faast-15nov24

--- svmesh/reject.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl label namespace knative-serving istio-injection-
-kubectl label namespace kourier-system istio-injection-
-kubectl -n knative-serving delete pod --all
-kubectl -n kourier-system delete pod --all
