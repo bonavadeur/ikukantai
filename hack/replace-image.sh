ff2e4bf946753a48f2ca209569099a21c3626a48
kenphunggg <kenphungstudy@gmail.com>
2024-10-16 06:18:42 -0700

    document v2.0

--- hack/replace-image.sh
+++ hack/replace-image.sh
@@ -7,7 +7,7 @@ kubectl -n knative-serving patch deploy net-kourier-controller --patch \
     '{"spec":{"template":{"spec":{"containers":[{"name":"controller","image":"docker.io/bonavadeur/ikukantai-kourier:'${TAG}'"}]}}}}'
 kubectl -n knative-serving patch deploy controller --patch \
     '{"spec":{"template":{"spec":{"containers":[{"name":"controller","image":"docker.io/bonavadeur/ikukantai-controller:'${TAG}'"}]}}}}'
-kubectl -n knative-serving patch deploy activator --patch \
+kubectl -n knative-serving patch daemonset activator --patch \
     '{"spec":{"template":{"spec":{"containers":[{"name":"activator","image":"docker.io/bonavadeur/ikukantai-activator:'${TAG}'"}]}}}}'
 kubectl -n knative-serving patch deploy autoscaler --patch \
     '{"spec":{"template":{"spec":{"containers":[{"name":"autoscaler","image":"docker.io/bonavadeur/ikukantai-autoscaler:'${TAG}'"}]}}}}'
@@ -20,4 +20,4 @@ kubectl -n knative-serving patch configmap config-deployment --patch \
 
 # replace image of miporin
 kubectl -n knative-serving patch deploy miporin --patch \
-    '{"spec":{"template":{"spec":{"containers":[{"name":"autoscaler","image":"docker.io/bonavadeur/miporin:'${TAG}'"}]}}}}'
+    '{"spec":{"template":{"spec":{"containers":[{"name":"miporin","image":"docker.io/bonavadeur/miporin:'${TAG}'"}]}}}}'
