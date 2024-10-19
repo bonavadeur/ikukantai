06873bdbacf5a1ea2bece6c529f8b5496a5fc726
Phùng Quang Thái <152577207+kenphunggg@users.noreply.github.com>
2024-10-19 13:25:54 +0700

    fix bugs when install metric collector (#8)

---
+++ hack/fix-otel-collector-cfg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kubectl patch configmap otel-collector-config -n metrics --type='merge' -p='{
+  "metadata": {
+    "annotations": {
+      "kubectl.kubernetes.io/last-applied-configuration": "{\"apiVersion\":\"v1\",\"data\":{\"collector.yaml\":\"receivers:\\n  opencensus:\\n    endpoint: \\\"0.0.0.0:55678\\\"\\n\\nexporters:\\n  debug:\\n  prometheus:\\n    endpoint: \\\"0.0.0.0:8889\\\"\\nextensions:\\n  health_check:\\n  pprof:\\n  zpages:\\nservice:\\n  extensions: [health_check, pprof, zpages]\\n  pipelines:\\n    metrics:\\n      receivers: [opencensus]\\n      processors: []\\n      exporters: [prometheus]\"},\"kind\":\"ConfigMap\",\"metadata\":{\"annotations\":{},\"name\":\"otel-collector-config\",\"namespace\":\"metrics\"}}"
+    }
+  }
+}'
+
+kubectl patch configmap otel-collector-config -n metrics --type='merge' -p='{
+  "data": {
+    "collector.yaml": "receivers:\n  opencensus:\n    endpoint: \"0.0.0.0:55678\"\n\nexporters:\n  debug:\n  prometheus:\n    endpoint: \"0.0.0.0:8889\"\nextensions:\n  health_check:\n  pprof:\n  zpages:\nservice:\n  extensions: [health_check, pprof, zpages]\n  pipelines:\n    metrics:\n      receivers: [opencensus]\n      processors: []\n      exporters: [prometheus]"
+  }
+}'
